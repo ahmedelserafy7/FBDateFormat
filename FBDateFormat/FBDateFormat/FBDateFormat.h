f9ff01ae677a13c616abe578d05af85a5781e9bf
ahmedelserafy7 <ahmed.samer43@yahoo.com>
2021-08-30 16:58:01 +0200

    add first commit

---
+++ FBDateFormat/FBDateFormat/FBDateFormat.h
@@ -0,0 +1,18 @@
+//
+//  FBDateFormat.h
+//  FBDateFormat
+//
+//  Created by Elser_10 on 8/30/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FBDateFormat.
+FOUNDATION_EXPORT double FBDateFormatVersionNumber;
+
+//! Project version string for FBDateFormat.
+FOUNDATION_EXPORT const unsigned char FBDateFormatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBDateFormat/PublicHeader.h>
+
+
